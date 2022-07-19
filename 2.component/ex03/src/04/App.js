04141f7c993b7968febc2f38e86b2067973cb56d
김현석 <gustjr324@gmail.com>
2022-07-19 11:01:09 +0900

    React Event finish

--- 2.component/ex03/src/04/App.js
+++ 2.component/ex03/src/04/App.js
@@ -2,6 +2,8 @@ import React, {Fragment, useRef} from 'react';
 import logo from '../assets/images/react-logo.png';
 
 export default function App() {
+    const imgRef= useRef(null);
+
     // const onKeyPressInput = e => {console.log(e.target.value)};
     const onKeyPressInput = e => {
         if(e.key === 'Enter') {
@@ -12,7 +14,9 @@ export default function App() {
     const onBlurInput = e=>{console.log("blur")}
     const onMouseOverImage = e=>{console.log('mouseover', `x=${e.clientX}, y=${e.clientY}`)}
     const onMouseMoveImage = e=>{
-        console.log('mousemove', `x=${e.clientX}, y=${e.clientY}`);
+        const offsetTop = imgRef.current.offsetTop;
+        const offsetLeft = imgRef.current.offsetLeft; 
+        console.log('mousemove', `x=${e.clientX-offsetLeft}, y=${e.clientY-offsetTop}`);
     }
     const onMouseOutImage = e=>{  console.log('mouseout', `x=${e.clientX}, y=${e.clientY}`);}
     const onMouseDownImage = e=>{console.log('mousedown', `x=${e.clientX}, y=${e.clientY}`);}
@@ -33,6 +37,7 @@ export default function App() {
                 <br/>
                 <br/>
             <img
+                ref = {imgRef}
                 style={ {
                     cursor: 'pointer',
                     width: 190,
