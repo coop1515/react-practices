33de2e3efa532589e8839b7066ebfcc01a014a19
김현석 <gustjr324@gmail.com>
2022-07-21 10:56:18 +0900

    fontawesome #2

--- kanbanboard/frontend/src/CardList.js
+++ kanbanboard/frontend/src/CardList.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './assets/css/CardList.css';
+import styles from'./assets/css/CardList.css';
 
 function CardList({title, cards}) {
     console.log(title,cards);
     return (
-        <div className={'CardList'}>
+        <div className={styles.CardList}>
             <h1>{title}</h1>
         </div>
     );
--- kanbanboard/frontend/src/KanbanBoard.js
+++ kanbanboard/frontend/src/KanbanBoard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import cards from './assets/json/data.json';
 import CardList from './CardList';
-import KabanBoard from './assets/css/KanbanBoard.css';
+import styles from './assets/css/KanbanBoard.css';
 
 function KanbanBoard() {
     
 
     return (
-        <div className={KabanBoard.KanbanBoard}>
+        <div className={styles.KanbanBoard}>
             <CardList title= {'ToDo'} cards = {cards.filter(e => e.status === 'ToDo')} />
             <CardList title= {'Doing'} cards = {cards.filter(e => e.status === 'Doing')} />
             <CardList title= {'Done'} cards = {cards.filter(e => e.status === 'Done')} />
