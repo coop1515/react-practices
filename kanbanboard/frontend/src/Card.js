5faec4f2babf64ea12ef0857b97c446d9209be2a
김현석 <gustjr324@gmail.com>
2022-07-25 10:10:32 +0900

    kanbanboard #4

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -2,15 +2,24 @@ import React, {useState} from 'react';
 import styles from'./assets/scss/Card.scss';
 import TaskList from './TaskList';
 function Card({card}) {
-    const {title, description, tasks} = card;
+    const {title, description, tasks,status} = card;
     const [showDetail, setShowDetail] = useState(false);
-
-    const onChangeshowDetail = e => {setShowDetail(showDetail => !showDetail)}
-    console.log(showDetail);
+    const styleSideColor = {
+        position: 'absolute',
+                zIndex: -1,
+                top: 0,
+                left: 0,
+                height: 100,
+                width: 4,
+                backgroundColor: status === 'ToDo' ? '#bd8D31' : (status === 'ToDo' ? '#3a7e28' : '#222')
+    }
+    const onClickshowDetail = e => {setShowDetail(showDetail => !showDetail)}
+    // console.log(showDetail);
     return (
         <div className={styles.Card}>
-            <div className={styles.Card__Title}
-                onClick = {onChangeshowDetail}>{title}</div>
+            <div style={styleSideColor} />
+            <div className={showDetail ? [styles.Card__Title,styles.Card__Title__Open].join(" ") : styles.Card__Title}
+                onClick = {onClickshowDetail}>{title}</div>
             
             {showDetail ? 
             <div className={styles.Card__Details}>
