6d250155cc64b1bd45784f6360805b494e9826e8
김현석 <gustjr324@gmail.com>
2022-07-26 12:07:49 +0900

    immutability #finish

--- 2.component/ex06/01.js
+++ 2.component/ex06/01.js
@@ -22,3 +22,16 @@ updateOrderProducts.push({
 console.log(state.order.products, updateOrderProducts, state.order.products === updateOrderProducts);
 
 console.log('===================================================');
+
+console.log('===================================================');
+state = {
+    order: JSON.parse(fs.readFileSync('./json/data.json','utf-8')) 
+}
+const updateOrderProducts3 = [...state.order.products,{
+    no: "p002-003",
+    name: "블루양말",
+    price: 1000,
+    amount: 1
+}];
+console.log(state.order.products, updateOrderProducts3, state.order.products === updateOrderProducts3);
+
